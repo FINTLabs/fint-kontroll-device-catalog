773cf77d4d6eb73a11b6c1cdc8a62efde861c0e3
Erling Jahr <erling.jahr@novari.no>
2025-12-02 14:11:37 +0100

    added oauth2 resourceserver

--- src/main/kotlin/no/novari/fintkontrolldevicecatalog/Application.kt
+++ src/main/kotlin/no/novari/fintkontrolldevicecatalog/Application.kt
@@ -3,13 +3,11 @@ package no.novari.fintkontrolldevicecatalog
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication(
     scanBasePackages = ["no.novari","no.fintlabs"]
 )
 @EnableScheduling
-@EnableWebSecurity
 class Application
 
 fun main(args: Array<String>) {
