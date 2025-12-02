47367a3519d65aab40ab468987ef50b136aa5f9d
Erling Jahr <erling.jahr@novari.no>
2025-12-02 20:46:11 +0100

    changed opa url in application.yaml

--- src/main/kotlin/no/novari/fintkontrolldevicecatalog/kontrollentity/KontrollEntityController.kt
+++ src/main/kotlin/no/novari/fintkontrolldevicecatalog/kontrollentity/KontrollEntityController.kt
@@ -1,7 +1,12 @@
 package no.novari.fintkontrolldevicecatalog.kontrollentity
 
+import no.fintlabs.opa.AuthorizationClient
+import no.novari.fintkontrolldevicecatalog.entity.DeviceGroup
+import no.vigoiks.resourceserver.security.FintJwtEndUserPrincipal
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,11 +17,13 @@ private val logger = LoggerFactory.getLogger(KontrollEntityController::class.jav
 @RestController
 @RequestMapping("/api")
 class KontrollEntityController(
-    val kontrollEntityService: KontrollEntityService
+    val kontrollEntityService: KontrollEntityService,
 ) {
 
     @GetMapping("/devicegroups")
-    fun getKontrollDeviceGroups() = kontrollEntityService.findAllGroups()
+    fun getKontrollDeviceGroups(): List<KontrollDeviceGroup> = kontrollEntityService.findAllGroups()
+
+
 
     @GetMapping("/devicegroups/{id}")
     fun getDeviceGroupByID(@PathVariable id: Long): ResponseEntity<KontrollDeviceGroup> =
