ffb933e364a18c28c6b49bbc74232f0ecb008e3c
Erling Jahr <erling.jahr@novari.no>
2025-12-02 12:27:15 +0100

    added annotation

--- src/main/kotlin/no/novari/fintkontrolldevicecatalog/Application.kt
+++ src/main/kotlin/no/novari/fintkontrolldevicecatalog/Application.kt
@@ -3,11 +3,13 @@ package no.novari.fintkontrolldevicecatalog
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication(
     scanBasePackages = ["no.novari","no.fintlabs"]
 )
 @EnableScheduling
+@EnableWebSecurity
 class Application
 
 fun main(args: Array<String>) {
